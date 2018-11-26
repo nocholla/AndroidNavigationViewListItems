9d5d3434159a074a785e64233bd512d16d98b1a9
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2018-11-25 18:50:34 -0800

    Added Gallery Grid

--- app/src/main/java/com/nocholla/navigationviewlistitems/adapter/list/GalleryListAdapter.java
+++ app/src/main/java/com/nocholla/navigationviewlistitems/adapter/list/GalleryListAdapter.java
@@ -37,7 +37,7 @@ public class GalleryListAdapter extends RecyclerView.Adapter<GalleryListAdapter.
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.gallery_thumbnail, parent, false);
+                .inflate(R.layout.gallery_thumbnail_list, parent, false);
 
         return new MyViewHolder(itemView);
     }
